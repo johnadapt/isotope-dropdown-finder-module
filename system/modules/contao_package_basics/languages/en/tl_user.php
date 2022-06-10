a2296514adaecb97f09edacce52d9275c8b6e0dc
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-06-10 15:00:30 -0400

    Create tl_user.php

---
+++ system/modules/contao_package_basics/languages/en/tl_user.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Add User Fields
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/add-user-fields
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_user']['user_image']          = array('Image', 'Custom "Image" User field.');
+$GLOBALS['TL_LANG']['tl_user']['user_image_size']     = array('Image Size', 'Pick a size for the user\'s image.');
+$GLOBALS['TL_LANG']['tl_user']['user_image_meta']     = array('Meta Data', 'Input meta data for the user\'s image');
+$GLOBALS['TL_LANG']['tl_user']['user_bio']            = array('Bio', 'Custom "Bio" User field.');
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_user']['add_user_fields_legend'] = 'Custom Fields\'s Settings';
